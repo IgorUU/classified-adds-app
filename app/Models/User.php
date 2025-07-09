792ad4c86dcdeb38cb7b227066794c0a102b3be9
Igor <igor@studiopresent.com>
2025-07-09 22:16:02 +0200

    Setup models and migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,13 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
+
+    public function ads()
+    {
+        return $this->hasMany(Ad::class);
+    }
 
     /**
      * The attributes that should be hidden for serialization.
