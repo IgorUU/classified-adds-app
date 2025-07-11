b82f88604d43546675fda2d1b2b92fa826b8c1a8
Igor <igor@studiopresent.com>
2025-07-11 19:45:52 +0200

    Add ads listing to profile view with edit and delete options; implement confirmation for deletion

--- resources/views/profile/ads/index.blade.php
+++ resources/views/profile/ads/index.blade.php
@@ -4,4 +4,20 @@
 <h1>My ads</h1>
 
 <a href="{{ route('profile.ads.create') }}">New ad</a>
+@foreach ($ads as $ad)
+<div>
+  <h2>{{ $ad->title }}</h2>
+  <p>Description: {{ $ad->description }}</p>
+  <p>Price {{ $ad->price }} RSD</p>
+  <a href="{{ route('profile.ads.edit', $ad) }}">Edit</a>
+
+  <form method="POST" action="{{ route('profile.ads.destroy', $ad) }}" onclick="return confirm('Are you sure you want to delete this ad?')">
+    @csrf
+    @method('DELETE')
+    <button type="submit">Delete</button>
+  </form>
+
+</div>
+
+@endforeach
 @endsection
